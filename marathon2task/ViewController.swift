ede6f7206a3dfc4d6c818fafc815d3bf73a04712
Vika <vg.dev31@gmail.com>
2024-11-05 21:32:22 +0400

    Initial Commit

---
+++ marathon2task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marathon2task
+//
+//  Created by Vika on 05.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
